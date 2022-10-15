8e2c298f04289cf8dcdbf8d4f98151be1f8590ed
mpedrak <michalpedrak@student.agh.edu.pl>
2022-10-15 12:50:32 +0200

    7

---
+++ src/main/java/agh/ics/oop/MapDirection.java
@@ -0,0 +1,6 @@
+package agh.ics.oop;
+
+public enum MapDirection
+{
+    NORTH, SOUTH, WEST, EAST
+}
--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,91 +1,14 @@
 package agh.ics.oop;
-import static java.lang.System.out;
-import agh.ics.oop.Direction;
-import java.util.Arrays;
-
 
 public class World
 {
     public static void main(String[] args)
     {
-        //out.println("system wystartował");
-        Direction tab[] = zmien(args);
-        run(tab);
-        //out.println("system zakończył działanie");
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
     }
-    /*
-    public static void run(String[] args)
-    {
-        out.println("zwierzak idzie do przodu");
-        int czy = 1;
-        for (String x: args)
-        {
-            if (czy == 1)
-                czy = 0;
-            else
-                out.print(", ");
-            out.print(x);
-        }
-        out.println();
-    }
-    */
-    public static void run(Direction[] args)
-    {
-        out.println("Start");
-        /*
-        for (String x: args)
-        {
-            String message = switch (x)
-            {
-                case "f" -> "Zwierzak idzie do przodu";
-                case "b" -> "Zwierzak idzie do tyłu";
-                case "r" -> "Zwierzak skręca w prawo";
-                case "l" -> "Zwierzak skręca w lewo";
-                default -> "7";
-            };
-            if (!message.equals("7"))
-                out.println(message);
 
-        }
-        */
-        for (Direction x: args)
-        {
-            String message = switch (x)
-                    {
-                        case f -> "Zwierzak idzie do przodu";
-                        case b -> "Zwierzak idzie do tyłu";
-                        case r -> "Zwierzak skręca w prawo";
-                        case l -> "Zwierzak skręca w lewo";
-                        default -> "7";
-                    };
-            if (!message.equals("7"))
-               out.println(message);
-        }
-
-        out.println("Stop");
-    }
-    public static Direction [] zmien(String[] args)
-    {
-        int li = 0;
-        for (String x: args)
-            li++;
-        Direction tab[] = new Direction[li];
-        li = 0;
-        for (String x: args)
-        {
-            Direction message = switch (x)
-            {
-                case "f" -> Direction.f;
-                case "b" -> Direction.b;
-                case "r" -> Direction.r;
-                case "l" -> Direction.l;
-                default -> Direction.g;
-            };
-            tab[li] = message;
-            li++;
-
-        }
-        return tab;
-
-    }
 }
